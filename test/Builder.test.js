7291bc2a09c0b85a6513729c3397e49dfaaa8ce1
Sam-Mauris Yong <mauris@hotmail.sg>
2017-10-21 01:47:58 +0100

    add test for building file

--- test/Builder.test.js
+++ test/Builder.test.js
@@ -1,5 +1,9 @@
 const Builder = require('../src/Builder');
 const Context = require('../src/Context');
+const Promise = require('bluebird');
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
 
 describe('Builder', () => {
   describe('constructor', () => {
@@ -22,6 +26,55 @@ describe('Builder', () => {
         const context = new Context('test', 'test');
         const builder = new Builder(context);
       }).toThrow();
+    });
+  });
+
+  describe('build', () => {
+    let storageLocation = '';
+    beforeEach((done) => {
+      fs.mkdtemp(path.join(os.tmpdir(), 'pager-'), (err, pathname) => {
+        storageLocation = pathname;
+        done();
+      });
+    })
+
+    afterEach((done) => {
+      fs.readdir(storageLocation, (err, files) => {
+        let promises = [];
+        files.forEach((file) => {
+          promises.push(new Promise((resolve) => {
+            fs.unlink(path.join(storageLocation, file), resolve);
+          }));
+        });
+
+        Promise
+          .all(promises)
+          .then(() => {
+            fs.rmdir(storageLocation);
+            done();
+          });
+      });
+    });
+
+    test('normal usage', (done) => {
+      const key = 'test';
+      const context = new Context(storageLocation, key);
+      const retriever = (pageNum) => {
+        if (pageNum > 1) {
+          return Promise.resolve([]);
+        }
+
+        return Promise.resolve([{title: 'test'}]);
+      }
+      const builder = new Builder(context, retriever);
+      builder.build()
+        .then(() => {
+          let location = path.join(storageLocation, key) + '-1';
+          fs.access(location, (err) => {
+            expect(err).toBeFalsy();
+            done();
+          });
+        });
     });
   });
 });
