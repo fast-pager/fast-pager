e198d5d3b7e8aad93e3702c8fb25e2eb922d84fe
Sam-Mauris Yong <mauris@hotmail.sg>
2017-10-21 02:21:52 +0100

    add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "extends": "airbnb-base/legacy",
+  "rules": {
+    "no-console": ["warn", { "allow": ["log"] }],
+    "no-underscore-dangle": "off"
+  },
+  "parserOptions": {
+    "sourceType": "module"
+  }
+};
