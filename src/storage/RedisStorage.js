6073731daabdfaa1fbeb172d818b426fa541f38e
Sam-Mauris Yong <mauris@hotmail.sg>
2017-11-06 08:30:40 +0000

    update redis storage

--- src/storage/RedisStorage.js
+++ src/storage/RedisStorage.js
@@ -1,20 +1,18 @@
-const redis = require('redis');
-
 function RedisStorage(redisClient, key) {
   this._client = redisClient;
   this._key = key;
 }
 
 RedisStorage.prototype.write = function write(page, data) {
-  return new Promise((resolve, reject) => {
-    this._client.set(this._key + "-" + page, JSON.stringify(data));
+  return new Promise((resolve) => {
+    this._client.set(this._key + '-' + page, JSON.stringify(data));
     resolve();
   });
 };
 
 RedisStorage.prototype.read = function read(page) {
   return new Promise((resolve, reject) => {
-    this._client.get(this._key + "-" + page, (err, reply) => {
+    this._client.get(this._key + '-' + page, (err, reply) => {
       if (err || !reply) {
         reject();
         return;
